3d4cbee7bb5642a91554842d386c20dc195c8438
Shola Ayeni <ayenisholah@yahoo.com>
2024-04-23 00:00:17 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
 };
 
 export default nextConfig;
