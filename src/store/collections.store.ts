e0007fada20de382f502ea855abef1e28aa7da0d
Shola Ayeni <ayenisholah@yahoo.com>
2024-04-30 21:30:52 +0100

    implement remove collection

--- src/store/collections.store.ts
+++ src/store/collections.store.ts
@@ -2,6 +2,9 @@ import { create } from 'zustand';
 import { persist, PersistOptions } from 'zustand/middleware';
 import { useSettingsState } from './settings.store';
 import { BidState, useBidStateStore } from './bid.store';
+import { IOffer } from '@/services/offers';
+import { useOfferStateStore } from './offer.store';
+import { toast } from 'react-toastify';
 
 export interface CollectionData {
   collectionSymbol: string;
@@ -21,7 +24,7 @@ export interface CollectionData {
 interface CollectionsState {
   collections: CollectionData[];
   addCollection: (collection: CollectionData) => boolean;
-  removeCollection: (index: number) => void;
+  removeCollection: (index: number) => Promise<void>;
   editCollection: (index: number, updatedCollection: CollectionData) => void;
 }
 
@@ -58,7 +61,7 @@ export const useCollectionsState = create<CollectionsState>()(
 
         return true;
       },
-      removeCollection: (index) => {
+      removeCollection: async (index) => {
         set((state) => ({
           collections: state.collections.filter((_, i) => i !== index),
         }));
@@ -66,6 +69,45 @@ export const useCollectionsState = create<CollectionsState>()(
         useBidStateStore.getState().setBidStates(
           useBidStateStore.getState().bidStates.filter((_, i) => i !== index)
         );
+        const { collections } = get()
+        const collection = collections.find((_, i) => i === index)
+
+        const collectionSymbol = collection?.collectionSymbol as string
+
+        const { fundingWif, tokenReceiveAddress, apiKey } = useSettingsState.getState();
+
+        const { cancelAll } = useOfferStateStore.getState()
+
+        const wif = collection?.fundingWalletWIF || fundingWif
+
+        const receiveAddress = collection?.tokenReceiveAddress || tokenReceiveAddress
+
+
+        try {
+          const url = `/api/offers?requestType=getCollectionOffers&tokenReceiveAddress=${receiveAddress}&collectionSymbol=${collectionSymbol}&apiKey=${apiKey}`;
+          const response = await fetch(url.toString(), {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const responseData: IOffer[] = await response.json();
+
+          const offers = responseData
+            .filter((item) => item.token.collectionSymbol === collectionSymbol)
+          const offerIds = offers.map((item) => item.id)
+
+          await cancelAll(offerIds, wif, apiKey, collectionSymbol)
+          toast.success("successfully removed collection and delete all offers")
+
+        } catch (error) {
+          console.log(error);
+        }
+
+
       },
       editCollection: (index, updatedCollection) =>
         set((state) => ({
